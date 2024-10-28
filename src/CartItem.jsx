a5b79edf37ba70c4169e7fbc6f484e39cbd450ef
Yuval Shpitzer <shpitzery10@gmail.com>
2024-10-28 19:18:28 +0200

    fixed checkout button

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -19,7 +19,7 @@ const CartItem = ({ onContinueShopping , addedToCart , setAddedToCart}) => {
   };
 
   const handleCheckoutShopping = (e) => {
-    alert('Functionality to be added for future reference');
+    alert('Coming Soon');
   };
 
   const handleIncrement = (item) => {
@@ -74,7 +74,7 @@ const CartItem = ({ onContinueShopping , addedToCart , setAddedToCart}) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
